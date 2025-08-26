2af50ba702259a8832a4af19ff50c7674bfae2b0
Isabela <paim.isabela@hotmail.com>
2025-08-26 16:32:14 -0300

    Implementação Finalizada

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,15 @@
 # Sua solução aqui
+a = int(input("Digite o valor de a: "))
+b = int ( input("Digite o valor de b: "))
+c = int (input ("Digite o valor de c: "))
+
+if a < (b+c) and b < (a+c) and c < (a+b):
+    if a == b and b == c :
+        print("equilátero")
+    elif a == b or b == c or a == c :
+        print("isósceles")
+    else :
+        print ("escaleno")
+
+else : 
+    print("Não forma triângulo")
