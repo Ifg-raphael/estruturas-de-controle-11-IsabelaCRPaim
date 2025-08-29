41ce598ccc8ec06dbd8699c885a099acbf9d6d04
Isabela <paim.isabela@hotmail.com>
2025-08-29 18:07:41 -0300

    Implementação Finalizada

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -3,6 +3,8 @@ a = int(input("Digite o valor de a: "))
 b = int ( input("Digite o valor de b: "))
 c = int (input ("Digite o valor de c: "))
 
+#Verificar se forma um triângulo e qual tipo de triângulo
+
 if a < (b+c) and b < (a+c) and c < (a+b):
     if a == b and b == c :
         print("equilátero")
